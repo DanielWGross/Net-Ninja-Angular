9eb36c2a3410a67b8adf80c81a62d40f5fc562e1
Daniel Gross <danwgross@gmail.com>
2018-11-03 12:56:14 -0400

    #16 Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DirectoryComponent } from './directory/directory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DirectoryComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
